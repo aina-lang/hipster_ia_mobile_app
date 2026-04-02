30902745bc0bcffcbdaa0cef70522553073f7ac1
leonie1601 <leonie@distributorsroad.com>
2026-04-02 17:46:31 +0300

    Ajout du texte Bon retour et validation zod sur le formulaire de connexion

---
+++ theme/commonStyles.ts
@@ -0,0 +1,8 @@
+import { colors } from './colors';
+
+export const neonTextGlow = {
+  color: '#fff',
+  textShadowColor: colors.neonBlue,
+  textShadowOffset: { width: 0, height: 0 },
+  textShadowRadius: 3,
+} as const;
